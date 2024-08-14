a916bd6ab4ff950ffe12fa416b7c28c7864fd273
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2024-08-14 15:51:13 -0600

    Got everything working again

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -14,10 +14,11 @@ cp model.project.json $SERVE_DIR/model.project.json
 cp serve.project.json $SERVE_DIR/serve.project.json
 cp -r src $SERVE_DIR/src
 cp -rL Packages $SERVE_DIR/Packages
-
+cp -rL node_modules $SERVE_DIR/node_modules
 rojo sourcemap model.project.json -o $SOURCEMAP
 
 rojo sourcemap --watch model.project.json -o $SOURCEMAP &
 darklua process src $SERVE_DIR/src --config $DARKLUA_CONFIG -w & 
+darklua process node_modules $SERVE_DIR/node_modules --config $DARKLUA_CONFIG -w & 
 
 rojo serve $SERVE_DIR/serve.project.json
