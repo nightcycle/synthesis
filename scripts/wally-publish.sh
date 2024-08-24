24d7bf506b00fe94937dedbddb546eddd94c42e4
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2024-08-24 02:14:27 -0600

    improvements to wally-publish workflow

--- scripts/wally-publish.sh
+++ scripts/wally-publish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 sh scripts/build.sh
+rm -rf build/model.project.json
+rm -rf build/build.project.json
+cp -r default.project.json build/default.project.json
 cd "build"
 wally publish
