e3a36982941f018b762be3a5678246a9b0af70d6
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2024-11-27 11:56:46 -0700

    removed npm references

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -14,11 +14,10 @@ cp model.project.json $SERVE_DIR/model.project.json
 cp build.project.json $SERVE_DIR/build.project.json
 cp -r src $SERVE_DIR/src
 cp -rL Packages $SERVE_DIR/Packages
-cp -rL node_modules $SERVE_DIR/node_modules
 rojo sourcemap model.project.json -o $SOURCEMAP
 rojo sourcemap build.project.json --output sourcemap.json
 
 rojo sourcemap --watch model.project.json -o $SOURCEMAP &
-darklua process src $SERVE_DIR/src --config $DARKLUA_CONFIG -w & 
+darklua process src $SERVE_DIR/src --config $DARKLUA_CONFIG -w &
 
 rojo serve $SERVE_DIR/build.project.json
